a52135d496411c0d0883a9a0dd37f17a2d8e31f3
Lucas Bighi <lucasmbighi@icloud.com>
2021-05-20 14:36:50 -0300

    Initial Commit

---
+++ Appzineo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appzineo
+//
+//  Created by Lucas Marques Bigh (P) on 20/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
